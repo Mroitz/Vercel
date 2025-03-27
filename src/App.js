9c4c66c479bfa9c95f703963db4e3544f2dc796e
Ian <ianlee.felder@gmail.com>
2025-03-27 10:32:23 +0100

    test commit ydd bugfix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App () {
     <div className='App'>
       <h1>mucho gusto</h1>
       <div>un te o un cafe?</div>
-      <button onClick={alert(hurensohn)}></button>
+      <button onClick={alert('hurensohn')}></button>
     </div>
   )
 }
